bced993ae880e0cde4bf5c21379bc83913c334a6
prikol769 <prikol769@gmail.com>
2024-04-02 13:40:26 -0500

    fix mobile scroll

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <main className="bg-black">
+    <main className="bg-black overflow-y-auto">
       <Navbar />
       <Hero />
       <Highlights />
