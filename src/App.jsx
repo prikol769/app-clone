d774fec0c25f60ed93708d0652a429d17c3bcbfd
prikol769 <prikol769@gmail.com>
2024-04-02 11:50:56 -0500

    added Model component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <main className="bg-black">
       <Navbar /> <Hero /> <Highlights />
+      <Model />
     </main>
   );
 };
